3eca5406ecbe29faf3f42772103e15b3c9022f02
Jacbdias <fatosdabolsa@gmail.com>
2025-05-22 18:10:55 -0300

    Update imposto.js

--- pages/imposto.js
+++ pages/imposto.js
@@ -1,10 +1,10 @@
 export default function Imposto() {
   const ativos = [
     { ativo: 'PETR4', setor: 'Petróleo', peso: '18%', logo: 'petr4.png' },
-    { ativo: 'ITUB4', setor: 'Energia', peso: '15%', logo: 'taee11.png' },
-    { ativo: 'BBSA3', setor: 'Financeiro', peso: '12%', logo: 'bbas3.png' },
-    { ativo: 'VALE3', setor: 'Financeiro', peso: '10%', logo: 'itsa4.png' },
-    { ativo: 'WEGE3', setor: 'Energia', peso: '8%', logo: 'egie3.png' }
+    { ativo: 'ITUB4', setor: 'Energia', peso: '15%', logo: 'itub4.png' },
+    { ativo: 'BBSA3', setor: 'Financeiro', peso: '12%', logo: 'bbsa3.png' },
+    { ativo: 'VALE3', setor: 'Financeiro', peso: '10%', logo: 'vale3.png' },
+    { ativo: 'WEGE3', setor: 'Energia', peso: '8%', logo: 'wege3.png' }
   ];
 
   return (
